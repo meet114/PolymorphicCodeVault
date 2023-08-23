4d5280be95d377faf15b995be37e051c7e0848bc
meet114 <meetsaspara9@gmail.com>
2023-08-23 14:35:58 -0600

    P01 Bonus: Added user-input and screenshot

---
+++ P01/bonus/Hello.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = input.next();
+        System.out.println();
+        System.out.println("Hello, " + name);
+    }
+}
