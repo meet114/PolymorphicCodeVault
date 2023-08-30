75ae14e20a89d8d34bfd2be2fe8ed4d5889cf624
meet114 <meetsaspara9@gmail.com>
2023-08-30 10:14:59 -0600

    Fixed AI.java & Added Setter & Getters for queries

--- P02/full_credit/AI.java
+++ P02/full_credit/AI.java
@@ -4,11 +4,26 @@ public class AI {
 
     private Engine engine;
     private String[] queries;
+    private final int querySize = 5;
 
     public AI(Engine engine) {
         this.engine = engine;
-        this.queries = new String[5];
+        this.queries = new String[querySize];
     }
 
-    public String query(String inQuery) {}
+    public String query(String inQuery) {
+        for (int i = querySize - 1; i > 0; i++) {
+            queries[i] = queries[i - 1];
+        }
+        queries[0] = inQuery;
+        return "AI (" + engine + ") processed: '" + inQuery + "'.";
+    }
+
+    public String[] getQueryHistory() {
+        return queries;
+    }
+
+    public void setQueryHistory(String[] queries) {
+        this.queries = queries;
+    }
 }
