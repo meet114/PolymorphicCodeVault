5c836d10be8aeeeb5e94ced2059b0cb0274d2e18
meet114 <meetsaspara9@gmail.com>
2023-08-29 22:20:50 -0600

    Added .gitignore to exclude .DS_Store files

---
+++ P02/full_credit/AI.java
@@ -0,0 +1,14 @@
+package P02.full_credit;
+
+public class AI {
+
+    private Engine engine;
+    private String[] queries;
+
+    public AI(Engine engine) {
+        this.engine = engine;
+        this.queries = new String[5];
+    }
+
+    public String query(String inQuery) {}
+}
---
+++ P02/full_credit/Engine.java
@@ -0,0 +1,7 @@
+package P02.full_credit;
+
+public enum Engine {
+    OPENAI,
+    GOOGLE_BARD,
+    IBM_WATSON,
+}
