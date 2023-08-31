2f97bd587dff7ffd9ba2e5a51b77b812a1328bd7
meet114 <meetsaspara9@gmail.com>
2023-08-30 18:54:03 -0600

    Query.java Updated

--- P02/full_credit/Query.java
+++ P02/full_credit/Query.java
@@ -1,3 +1,39 @@
 package P02.full_credit;
 
-public class Query {}
+import java.util.Scanner;
+
+public class Query {
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println(" AI engines can be use are:");
+            for (Engine engine : Engine.values()) {
+                System.out.println(engine);
+            }
+            System.out.println(
+                "Engines can be run after: Java P02.full_credit.Query <Engine_name.>"
+            );
+            return;
+        }
+        String engineOn = args[0].toUpperCase();
+        boolean weHaveEngine = false;
+
+        for (Engine engine : Engine.values()) {
+            if (engine.name().equals(engineOn)) {
+                weHaveEngine = true;
+            }
+        }
+        if (weHaveEngine == false) {
+            System.out.println("Sorry we don't have " + engineOn);
+            System.out.println("but you can use:");
+            for (Engine engine : Engine.values()) {
+                System.out.println(engine);
+            }
+            return;
+        }
+        Engine engineRunning = Engine.valueOf(engineOn);
+        AI ai = new AI(engineRunning);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Start://d");
+    }
+}
