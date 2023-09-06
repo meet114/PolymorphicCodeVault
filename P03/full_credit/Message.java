45089e165b6b286f101c4e5480d61b09ce328452
meet114 <meetsaspara9@gmail.com>
2023-09-06 13:23:47 -0600

    Completed Message class without toString.

--- P03/full_credit/Message.java
+++ P03/full_credit/Message.java
@@ -4,8 +4,42 @@ import java.util.Date;
 public class Message {
 
     private Account from;
+    private Date date;
     private Message repliedTo;
-    private String body;
-    private Date timestamp;
     private ArrayList<Message> replies;
+    private String body;
+
+    public Message(Account from, Message repliedTo, String body) {
+        this.from = from;
+        this.repliedTo = repliedTo;
+        this.body = body;
+        this.date = new Date();
+        this.replies = new ArrayList<>();
+
+        if (repliedTo != null) {
+            repliedTo.addReply(this);
+        }
+    }
+
+    private void addReply(Message msg) {
+        replies.add(msg);
+    }
+
+    public Message getRepliedTo() {
+        return repliedTo;
+    }
+
+    public Message getReply(int index) {
+        if (index < 0 || index >= replies.size()) {
+            return null;
+        }
+        return replies.get(index);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        return sb.toString();
+    }
 }
