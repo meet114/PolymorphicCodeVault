d20b726e09da6e96342ae0a060ac1f8cfaaa9779
meet114 <meetsaspara9@gmail.com>
2023-09-13 17:02:34 -0600

    copied from P03/bonus and tested.

---
+++ P04/account/AccountStatus.java
@@ -0,0 +1,7 @@
+package account;
+
+public enum AccountStatus {
+    Normal, // Account is fully functional
+    Muted, // Can see posts but their own posts are hidden
+    Blocked, // Cannot log in
+}
