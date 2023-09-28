6ef9babb51b14c1eda8ac573003ba991cfe84b13
meet114 <meetsaspara9@gmail.com>
2023-09-28 12:27:19 -0600

    Create Abuta class and constructor.

--- P05/abuta/Abuta.java
+++ P05/abuta/Abuta.java
@@ -1,1 +1,26 @@
-public class Abuta {}
+package abuta;
+
+import account.*;
+import java.util.ArrayList;
+import java.util.List;
+import menu.*;
+import message.*;
+
+public class Abuta {
+
+    private List<Account> accounts;
+    private List<Group> groups;
+    private Message message;
+    private Menu menu;
+    private String output;
+    private boolean running;
+
+    public Abuta() {
+        this.running = true;
+        this.output = "";
+
+        // Instantiate lists as ArrayLists (as required)
+        this.accounts = new ArrayList<>();
+        this.groups = new ArrayList<>();
+    }
+}
