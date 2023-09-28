3627bceee4c61636e5dfc056013e63923dc30808
meet114 <meetsaspara9@gmail.com>
2023-09-28 12:39:47 -0600

    updatd Abuta class Constructor.

--- P05/abuta/Abuta.java
+++ P05/abuta/Abuta.java
@@ -21,5 +21,33 @@ public class Abuta {
 
         this.accounts = new ArrayList<>();
         this.groups = new ArrayList<>();
+        accounts.add(new Account("Meet"));
+        accounts.add(new Account("Jay"));
+        accounts.add(new Account("Manit"));
+        accounts.add(new Account("Ved"));
+        accounts.add(new Account("Prof Rice"));
+        groups.add(new Group("Gaming Legends"));
+        groups.add(new Group("Science Wizards"));
+        groups.add(new Group("Movie Buffs"));
+        groups.add(new Group("Music Maestros"));
+        groups.add(new Group("Tech Enthusiasts"));
+
+        this.message =
+            new Post(accounts.get(0), groups.get(0), "Welcome to the group!");
+        menu = new Menu();
+        menu.addMenuItem(new MenuItem("Exit", this::endApp));
+        menu.addMenuItem(new MenuItem("Show Reply", this::showReply));
+        menu.addMenuItem(new MenuItem("Show Replied To", this::showRepliedTo));
+        menu.addMenuItem(new MenuItem("Add Reply", this::reply));
     }
+
+    private void endApp() {
+        running = false;
+    }
+
+    private void showRepliedTo() {}
+
+    private void showReply() {}
+
+    private void reply() {}
 }
