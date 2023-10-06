182928615492c3243534e848f005a20e904c0886
meet114 <meetsaspara9@gmail.com>
2023-10-05 21:54:46 -0600

    Added Javadoc from P04

--- P06/abuta/Abuta.java
+++ P06/abuta/Abuta.java
@@ -11,6 +11,21 @@ import java.util.List;
 import menu.*;
 import message.*;
 
+/**
+ * The {@code Abuta} class serves as the main controller for the abUTA social media application.
+ * It manages user accounts, groups, and the message tree. This class provides an interactive menu
+ * with options to navigate and update the message tree (add replies, show parent/child messages),
+ * modify account and group statuses, and perform file I/O operations (save, save as, open, and reset
+ * the message tree via newAbuta()).
+ *
+ * <p>The class follows the encapsulated save/open pattern where each data class (Account, Group,
+ * Message, Post, DirectMessage) is responsible for saving and restoring its own state.
+ * The menu options are built using the {@code Menu} and {@code MenuItem} classes.
+ *
+ * @author Meetkumar Saspara
+ * @version 1.0
+ * @since 2025
+ */
 public class Abuta {
 
     private List<Account> accounts;
@@ -21,6 +36,14 @@ public class Abuta {
     private boolean running;
     private String filename = "default.txt";
 
+    /**
+     * Constructs a new Abuta application.
+     * <p>
+     * Initializes the accounts, groups, and creates an initial message.
+     * It also sets up the menu with options including exit, navigation,
+     * adding replies/accounts/groups, muting accounts, toggling group status,
+     * and file I/O operations (newAbUTA, save, saveAs, open).
+     */
     public Abuta() {
         this.running = true;
         this.output = "";
@@ -40,6 +63,7 @@ public class Abuta {
         groups.add(new Group("Music Maestros"));
         groups.add(new Group("Tech Enthusiasts"));
 
+        // Create the initial message.
         this.message =
             new Post(accounts.get(0), groups.get(0), "Welcome to the group!");
 
@@ -65,6 +89,11 @@ public class Abuta {
         menu.addMenuItem(new MenuItem("Open", this::open));
     }
 
+    /**
+     * Starts the main display loop of the application.
+     * <p>
+     * Continuously displays the menu and processes user input until the application is terminated.
+     */
     public void mdi() {
         while (running) {
             System.out.println("=== abUTA Social Media ===");
@@ -79,6 +108,10 @@ public class Abuta {
         }
     }
 
+    /**
+     * Displays the parent message of the current message.
+     * If there is no parent message, sets output to indicate that.
+     */
     private void showRepliedTo() {
         if (message.getRepliedTo() == null) {
             output = "No parent message.";
@@ -87,6 +120,11 @@ public class Abuta {
         }
     }
 
+    /**
+     * Displays one of the replies to the current message.
+     * If there is exactly one reply, that reply is shown;
+     * if there are multiple replies, the user is prompted to choose one.
+     */
     private void showReply() {
         int numReplies = message.getNumReplies();
 
@@ -111,10 +149,21 @@ public class Abuta {
         }
     }
 
+    /**
+     * Terminates the application.
+     */
     private void endApp() {
         running = false;
     }
 
+    /**
+     * Prompts the user to add a new reply to the current message.
+     * Depending on the user's input, a new Post or DirectMessage is created.
+     * <p>
+     * If the user chooses a Post, they must select an author and group and enter a message.
+     * If the user chooses a DirectMessage, they must select an author and recipient and enter a message.
+     * The new reply is then attached to the current message.
+     */
     private void reply() {
         char type = Menu.getChar("Post (P) or Direct Message (D)?");
 
@@ -142,6 +191,8 @@ public class Abuta {
                 return;
             }
 
+            // Create a new Post; if your UML specifies linking replies,
+            // you may need to pass the current message as the repliedTo parameter.
             newMessage = new Post(author, group, body);
         } else {
             int recipientIndex = Menu.selectItemFromList(
@@ -166,6 +217,9 @@ public class Abuta {
         output = "Reply added successfully.";
     }
 
+    /**
+     * Prompts the user to enter a new account name and adds the account to the system.
+     */
     private void addAccount() {
         String name = Menu.getString("Enter new account name:").trim();
         if (name.isEmpty()) {
@@ -177,6 +231,9 @@ public class Abuta {
         output = "Account '" + name + "' added successfully.";
     }
 
+    /**
+     * Prompts the user to enter a new group name and adds the group to the system.
+     */
     private void addGroup() {
         String name = Menu.getString("Enter new group name:").trim();
         if (name.isEmpty()) {
@@ -188,6 +245,9 @@ public class Abuta {
         output = "Group '" + name + "' added successfully.";
     }
 
+    /**
+     * Toggles the mute status of an account selected by the user.
+     */
     private void toggleMuteAccount() {
         int accountIndex = Menu.selectItemFromList(
             "Select an account to mute/unmute:",
@@ -209,6 +269,9 @@ public class Abuta {
         }
     }
 
+    /**
+     * Toggles the active status of a group selected by the user.
+     */
     private void toggleGroupStatus() {
         int groupIndex = Menu.selectItemFromList(
             "Select a group to enable/disable:",
@@ -230,10 +293,11 @@ public class Abuta {
         }
     }
 
-    public static void main(String[] args) {
-        new Abuta().mdi();
-    }
-
+    /**
+     * Resets the current message to the root of the message tree.
+     * <p>
+     * Traverses upward using {@code getRepliedTo()} until the root is found.
+     */
     public void newAbuta() {
         Message root = message;
         while (root.getRepliedTo() != null) {
@@ -242,6 +306,13 @@ public class Abuta {
         message = root;
     }
 
+    /**
+     * Saves the current message tree to a file.
+     * <p>
+     * Uses a try-with-resources block to open a {@code BufferedWriter} using the current filename.
+     * It finds the root message by traversing upward and then calls {@code save(BufferedWriter)}
+     * on that root.
+     */
     public void save() {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename))) {
             Message root = message;
@@ -254,6 +325,11 @@ public class Abuta {
         }
     }
 
+    /**
+     * Saves the current message tree to a new file.
+     * <p>
+     * Prompts the user for a new filename, updates the filename, and then calls {@code save()}.
+     */
     public void saveAs() {
         String newFilename = Menu.getString("Enter new filename: ");
         if (newFilename != null && !newFilename.isEmpty()) {
@@ -262,6 +338,13 @@ public class Abuta {
         }
     }
 
+    /**
+     * Opens a file and loads the message tree from it.
+     * <p>
+     * Prompts the user for a filename, then uses a try-with-resources block to open a
+     * {@code BufferedReader} with that filename and constructs a new {@code Post} (as the root message)
+     * from the file.
+     */
     public void open() {
         String fileToOpen = Menu.getString("Enter filename to open: ");
         if (fileToOpen != null && !fileToOpen.isEmpty()) {
@@ -274,5 +357,14 @@ public class Abuta {
                 e.printStackTrace();
             }
         }
+    }
+
+    /**
+     * The main method to start the abUTA Social Media application.
+     *
+     * @param args command-line arguments (not used)
+     */
+    public static void main(String[] args) {
+        new Abuta().mdi();
     }
 }
